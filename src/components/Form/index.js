6b19b10b23d985d5811adb2b531265a3efe1d165
Renato Almeida <renatoam@ymail.com>
2023-06-01 23:47:35 -0300

    feat: insercao de campos novos no form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,32 @@
 import './Form.css'
 import Textfield from '../Textfield';
+import SelectionList from '../SelectionList';
+import Button from '../Button'
+
 
 const Form = (props,children) => {
+  const teams = [
+    'Front-end',
+    'Back-end',
+    'Arquitetura',
+    'Devops',
+    'QA',
+    'Scrum'
+  ]
+  const onSave = (evt) => {
+    evt.preventDefault();
+    console.log("Log de Form")
+    console.log(evt)
+  }
   return (
     <section className='form'>
-        <form>
+        <form onSubmit={onSave}>
             <h2>Preencha os dados abaixo para incluir um novo colaborador:</h2>
             <Textfield label="Nome" placeholder="Digite seu Nome"/>
             <Textfield label="Cargo" placeholder="Digite seu cargo"/>
             <Textfield label="Imagem" placeholder="Informe a URL da imagem"/>
+            <SelectionList label="Time" itens={teams} />
+            <Button>Criar Card</Button>
         </form>
     </section>
   );
