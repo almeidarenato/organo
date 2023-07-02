89e47d6bc6faf9fb66d1789c5ca4d9268cea4629
Renato Almeida <renatoam@ymail.com>
2023-07-02 18:06:35 -0300

    feat: adicao de cor de fundo

--- src/components/Employee/index.js
+++ src/components/Employee/index.js
@@ -1,9 +1,11 @@
 import './Employee.css';
 
 
-const Employee = ({nome,cargo,imagem}) =>{
+const Employee = ({nome,cargo,imagem,corDeFundo}) =>{
     return (<div className='colaborador'>
-        <div className='cabecalho'><img src={imagem} alt={nome}/></div>
+        <div className='cabecalho' style={{backgroundColor: corDeFundo}}>
+            <img src={imagem} alt={nome} />
+        </div>
         <div className='rodape'>
             <h4>{nome}</h4>
             <h5>{cargo}</h5>
